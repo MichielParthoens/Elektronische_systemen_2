8b24b126be30c6edb92a27b61af5f1a4278c0b87
MichielParthoens <12203423@student.pxl.be>
2024-03-12 17:10:44 +0100

    added code for serial plotter in arduino

--- programming files/pedalshield code/clean_signal_conversion/clean_signal_conversion.ino
+++ programming files/pedalshield code/clean_signal_conversion/clean_signal_conversion.ino
@@ -9,8 +9,13 @@ int LED = 3;
 int FOOTSWITCH = 7; 
 int TOGGLE = 2; 
 
+const int lowerlimit = 0;
+const int upperlimit = 4000;
+
 void setup()
 {
+  //serial com for plotter
+    Serial.begin(9600);
   //ADC Configuration
   ADC->ADC_MR |= 0x80;   // DAC in free running mode.
   ADC->ADC_CR=2;         // Starts ADC conversion.
@@ -30,14 +35,30 @@ void loop()
   POT0=ADC->ADC_CDR[10];                 // read data from ADC8        
   POT1=ADC->ADC_CDR[11];                 // read data from ADC9   
   POT2=ADC->ADC_CDR[12];                 // read data from ADC10 
+
+Serial.print("Lowerlimit:");
+Serial.println(lowerlimit); // To freeze the lower limit
+Serial.print(",");
+Serial.print("Upperlimit:");
+Serial.println(upperlimit); // To freeze the upper limit
+  //print the input values of adc0 and adc1
+  Serial.print(",");
+  Serial.print("adc_0:");
+  Serial.println(in_ADC0);
+  //Serial.print(",");
+  //Serial.print("adc_1:");
+  //Serial.println(in_ADC1);
     
   //Add volume feature with POT2
   out_DAC0=map(in_ADC0,0,4095,1,POT2);
   out_DAC1=map(in_ADC1,0,4095,1,POT2);
-  
+    Serial.print(",");
+  Serial.print("adc_0_amplified:");
+  Serial.println(out_DAC0);
   //Write the DACs
   dacc_set_channel_selection(DACC_INTERFACE, 0);       //select DAC channel 0
   dacc_write_conversion_data(DACC_INTERFACE, out_DAC0);//write on DAC
   dacc_set_channel_selection(DACC_INTERFACE, 1);       //select DAC channel 1
   dacc_write_conversion_data(DACC_INTERFACE, out_DAC1);//write on DAC
+  
 }
