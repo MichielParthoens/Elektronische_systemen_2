241aa52c51cfa9e8170abd1f9fffb14d6e252ebf
MichielParthoens <12203423@student.pxl.be>
2024-04-23 16:44:45 +0200

    code files updated

--- programming files/pedalshield code/clean_signal_conversion/clean_signal_conversion.ino
+++ programming files/pedalshield code/clean_signal_conversion/clean_signal_conversion.ino
@@ -9,8 +9,8 @@ int LED = 3;
 int FOOTSWITCH = 7;
 int TOGGLE = 2;
 
-const int lowerlimit = 1500;
-const int upperlimit = 2500;
+const int lowerlimit = 0;
+const int upperlimit = 5000;
 
 void setup()
 {
@@ -22,14 +22,14 @@ void setup()
   ADC->ADC_CHER = 0x1CC0; // Enable ADC channels 0 and 1.
 
   //DAC Configuration
-<<<<<<< HEAD
+
   analogWrite(DAC0,0);  // Enables DAC0
   analogWrite(DAC1,0);  // Enables DAC0
   analogWrite(55, 168);
-=======
+
   analogWrite(DAC0, 0); // Enables DAC0
   analogWrite(DAC1, 0); // Enables DAC0
->>>>>>> 9be1e5fe33dce2481a03040dfb6c02c42466f879
+
 }
 
 void loop()
@@ -42,19 +42,18 @@ void loop()
   POT1 = ADC->ADC_CDR[11];               // read data from ADC9
   POT2 = ADC->ADC_CDR[12];               // read data from ADC10
 
-<<<<<<< HEAD
+
 //Serial.print("Lowerlimit:");
 //Serial.println(lowerlimit); // To freeze the lower limit
 //Serial.print(",");
 //Serial.print("Upperlimit:");
 //Serial.println(upperlimit); // To freeze the upper limit
-=======
   Serial.print("Lowerlimit:");
+  
   Serial.println(lowerlimit); // To freeze the lower limit
   Serial.print(",");
   Serial.print("Upperlimit:");
   Serial.println(upperlimit); // To freeze the upper limit
->>>>>>> 9be1e5fe33dce2481a03040dfb6c02c42466f879
   //print the input values of adc0 and adc1
   //Serial.print(",");
   Serial.print("adc_0:");
@@ -64,19 +63,16 @@ void loop()
   //Serial.println(in_ADC1);
 
   //Add volume feature with POT2
-<<<<<<< HEAD
   out_DAC0=map(in_ADC0,0,4095,1,POT2);
   out_DAC1=map(in_ADC1,0,4095,1,POT2);
    // Serial.print(",");
   //Serial.print("adc_0_amplified:");
   //Serial.println(out_DAC0);
-=======
   out_DAC0 = map(in_ADC0, 0, 4095, 1, POT2);
   out_DAC1 = map(in_ADC1, 0, 4095, 1, POT2);
-  Serial.print(",");
-  Serial.print("adc_0_amplified:");
-  Serial.println(out_DAC0);
->>>>>>> 9be1e5fe33dce2481a03040dfb6c02c42466f879
+  //Serial.print(",");
+  //Serial.print("adc_0_amplified:");
+  //Serial.println(out_DAC0);
   //Write the DACs
   dacc_set_channel_selection(DACC_INTERFACE, 0);       //select DAC channel 0
   dacc_write_conversion_data(DACC_INTERFACE, out_DAC0);//write on DAC
